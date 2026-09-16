dcd26c731e87873861ed1e66d4b27e00428f8346
Syed Izyan Ali <syedizyanali12@gmail.com>
2026-09-17 02:11:15 +0500

    Added The Conversion Verification Program

---
+++ verification.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+int A = 8;
+int result1 = A<<2;
+int result2 = A>>1;
+
+printf("Result of A<<2 is: %d",result1);
+printf("\nResult of A>>1 is: %d",result2);
+
+    return 0;
+}
